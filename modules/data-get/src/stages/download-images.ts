9ec10d868c7d3d1c7032cb6d7e6433b1760ff741
kiootic <kiootic@gmail.com>
2018-06-14 21:29:21 +0800

    Improve image download performance

--- modules/data-get/src/stages/download-images.ts
+++ modules/data-get/src/stages/download-images.ts
@@ -1,7 +1,5 @@
 import Axios from 'axios';
 import { padStart } from 'lodash';
-import { join } from 'path';
-import { sync as rimraf } from 'rimraf';
 import { allWithProgress, exists, formatJson, mkdir, queueWork, readFile, writeTo } from '../common';
 import { decodeTex } from '../texture';
 /* tslint:disable:no-bitwise */
@@ -23,9 +21,6 @@ export async function downloadImages(rootPath: string, dataUrl: string) {
 
   rootPath = mkdir(rootPath, 'images');
   mkdir(rootPath, 'cache');
-  rimraf(join(rootPath, 'raw'));
-  rimraf(join(rootPath, 'cards'));
-  rimraf(join(rootPath, 'mons'));
   mkdir(rootPath, 'raw');
   mkdir(rootPath, 'cards');
   mkdir(rootPath, 'mons');
@@ -59,12 +54,17 @@ export async function downloadImages(rootPath: string, dataUrl: string) {
     const key = `${isCards ? 'cards' : 'mons'}_${padStart(id.toString(), 3, '0')}`;
     entries.push({ key, isCards, id, width, height, frames, lastUpdate });
   }
-  writeTo(formatJson(entries), rootPath, 'extlist.json');
+  const oldEntries: Entry[] = exists(rootPath, 'extlist.json') ?
+    JSON.parse((await readFile(rootPath, 'extlist.json')).toString('utf8')) : [];
 
   function extractETag(etag: string) {
     return /^(?:W\/)?"([^"]+)"$/.exec(etag)![1];
   }
   const tasks = entries.map(entry => queueWork(async () => {
+    const oldEntry = oldEntries.find(e => e.key === entry.key);
+    if (oldEntry && oldEntry.lastUpdate === entry.lastUpdate)
+      return null;
+
     const respHead = await Axios.head(`${entry.key}.bc`, { baseURL: dataUrl });
     let etag = extractETag(respHead.headers.etag);
     let data: Buffer;
@@ -84,6 +84,7 @@ export async function downloadImages(rootPath: string, dataUrl: string) {
     await writeTo(data, rootPath, 'raw', `${entry.key}.bc`);
     return data;
   }).then(async data => {
+    if (!data) return;
     try {
       const texs = await decodeTex(data, entry);
       for (const name of Object.keys(texs)) {
